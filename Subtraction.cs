918abe0059bb3b90efecb3312849493a892d3504
MD.Jahid Hassan <32840051+polashbabu@users.noreply.github.com>
2019-02-24 17:26:02 +0600

    Add files via upload

---
+++ Subtraction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace String_Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Now can write your name ");
+           float number1 =float.Parse(Console.ReadLine());
+            float number2 = float.Parse(Console.ReadLine());
+            float minamal = number1 - number2;
+            Console.WriteLine("number is  :"+minamal);
+            Console.ReadKey();
+            
+        }
+    }
+}
